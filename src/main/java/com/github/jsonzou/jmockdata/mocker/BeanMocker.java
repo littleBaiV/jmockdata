760f16772991b2a12c817776f232d97c511f3df5
拜力文 <bailiwen@worktrans.cn>
2020-06-30 11:23:02 +0800

    忽略静态字段

--- src/main/java/com/github/jsonzou/jmockdata/mocker/BeanMocker.java
+++ src/main/java/com/github/jsonzou/jmockdata/mocker/BeanMocker.java
@@ -9,92 +9,99 @@ import com.github.jsonzou.jmockdata.util.ReflectionUtils;
 import java.beans.IntrospectionException;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.util.Map.Entry;
 
 public class BeanMocker implements Mocker<Object> {
 
-  private final Class clazz;
+    private final Class clazz;
 
-  BeanMocker(Class clazz) {
-    this.clazz = clazz;
-  }
-
-  @Override
-  public Object mock(DataConfig mockConfig) {
-
-    try {
-      // fixme 解决方案不够优雅
-      if (mockConfig.globalConfig().isEnabledCircle()) {
-        Object cacheBean = mockConfig.globalConfig().getcacheBean(clazz.getName());
-        if (cacheBean != null) {
-          return cacheBean;
-        }
-      }
-      Object result = clazz.newInstance();
-      mockConfig.globalConfig().cacheBean(clazz.getName(), result);
-      /**
-       * 是否配置排除整个类
-       */
-      if(mockConfig.globalConfig().isConfigExcludeMock(clazz)){
-        return result;
-      }
-      setFieldValueByFieldAccessible(mockConfig, result);
-      return result;
-    } catch (Exception e) {
-      throw new MockException(e);
+    BeanMocker(Class clazz) {
+        this.clazz = clazz;
     }
-  }
 
-  /**
-   * 反射设置属性值
-   * @param mockConfig
-   * @param result
-   * @throws IllegalAccessException
-   */
-  private void setFieldValueByFieldAccessible(DataConfig mockConfig, Object result) throws IllegalAccessException{
-    for (Class<?> currentClass = clazz; currentClass != Object.class; currentClass = currentClass.getSuperclass()) {
-      // 模拟有setter方法的字段
-      for (Field field :currentClass.getDeclaredFields()) {
-        if (field.isAnnotationPresent(MockIgnore.class)) {
-          continue;
-        }
-        /**
-         * 是否配置排除这个属性
-         */
-        if(mockConfig.globalConfig().isConfigExcludeMock(clazz,field.getName())){
-          continue;
-        }
-        ReflectionUtils.setFieldValue(result, field, new BaseMocker(field.getGenericType()).mock(mockConfig.globalConfig().getDataConfig(currentClass,field.getName())));
-      }
-    }
-  }
+    @Override
+    public Object mock(DataConfig mockConfig) {
 
-  /**
-   * 内省设置属性值
-   * @param mockConfig
-   * @param result
-   * @throws IntrospectionException
-   * @throws ReflectiveOperationException
-   */
-  @Deprecated
-  private void setFieldValueByIntrospector(DataConfig mockConfig, Object result) throws IntrospectionException, ReflectiveOperationException {
-    for (Class<?> currentClass = clazz; currentClass != Object.class; currentClass = currentClass.getSuperclass()) {
-      // 模拟有setter方法的字段
-      for (Entry<Field, Method> entry : ReflectionUtils.fieldAndSetterMethod(currentClass).entrySet()) {
-        Field field = entry.getKey();
-        if (field.isAnnotationPresent(MockIgnore.class)) {
-          continue;
+        try {
+            // fixme 解决方案不够优雅
+            if (mockConfig.globalConfig().isEnabledCircle()) {
+                Object cacheBean = mockConfig.globalConfig().getcacheBean(clazz.getName());
+                if (cacheBean != null) {
+                    return cacheBean;
+                }
+            }
+            Object result = clazz.newInstance();
+            mockConfig.globalConfig().cacheBean(clazz.getName(), result);
+            /**
+             * 是否配置排除整个类
+             */
+            if (mockConfig.globalConfig().isConfigExcludeMock(clazz)) {
+                return result;
+            }
+            setFieldValueByFieldAccessible(mockConfig, result);
+            return result;
+        } catch (Exception e) {
+            throw new MockException(e);
         }
-        /**
-         * 是否配置排除这个属性
-         */
-        if(mockConfig.globalConfig().isConfigExcludeMock(clazz,field.getName())){
-           continue;
-        }
-        ReflectionUtils
-            .setRefValue(result, entry.getValue(), new BaseMocker(field.getGenericType()).mock(mockConfig.globalConfig().getDataConfig(currentClass,field.getName())));
-      }
     }
-  }
+
+    /**
+     * 反射设置属性值
+     *
+     * @param mockConfig
+     * @param result
+     * @throws IllegalAccessException
+     */
+    private void setFieldValueByFieldAccessible(DataConfig mockConfig, Object result) throws IllegalAccessException {
+        for (Class<?> currentClass = clazz; currentClass != Object.class; currentClass = currentClass.getSuperclass()) {
+            // 模拟有setter方法的字段
+            for (Field field : currentClass.getDeclaredFields()) {
+                int modifiers = field.getModifiers();
+                if (Modifier.isStatic(modifiers)) {
+                    continue;
+                }
+                if (field.isAnnotationPresent(MockIgnore.class)) {
+                    continue;
+                }
+                /**
+                 * 是否配置排除这个属性
+                 */
+                if (mockConfig.globalConfig().isConfigExcludeMock(clazz, field.getName())) {
+                    continue;
+                }
+                ReflectionUtils.setFieldValue(result, field, new BaseMocker(field.getGenericType()).mock(mockConfig.globalConfig().getDataConfig(currentClass, field.getName())));
+            }
+        }
+    }
+
+    /**
+     * 内省设置属性值
+     *
+     * @param mockConfig
+     * @param result
+     * @throws IntrospectionException
+     * @throws ReflectiveOperationException
+     */
+    @Deprecated
+    private void setFieldValueByIntrospector(DataConfig mockConfig, Object result) throws IntrospectionException, ReflectiveOperationException {
+        for (Class<?> currentClass = clazz; currentClass != Object.class; currentClass = currentClass.getSuperclass()) {
+            // 模拟有setter方法的字段
+            for (Entry<Field, Method> entry : ReflectionUtils.fieldAndSetterMethod(currentClass).entrySet()) {
+                Field field = entry.getKey();
+                if (field.isAnnotationPresent(MockIgnore.class)) {
+                    continue;
+                }
+                /**
+                 * 是否配置排除这个属性
+                 */
+                if (mockConfig.globalConfig().isConfigExcludeMock(clazz, field.getName())) {
+                    continue;
+                }
+                ReflectionUtils
+                        .setRefValue(result, entry.getValue(), new BaseMocker(field.getGenericType()).mock(mockConfig.globalConfig().getDataConfig(currentClass, field.getName())));
+            }
+        }
+    }
 
 }
